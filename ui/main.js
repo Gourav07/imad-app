b24c2a5d6cb7ef9f84234d9c6746e844f0225786
Gourav07 <gaurav07sharma1996@gmail.com>
2018-02-21 16:40:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
     
-};
+}
 img.onclick=function(){
    var interval=setInteraval(moveRight,100);
 };
